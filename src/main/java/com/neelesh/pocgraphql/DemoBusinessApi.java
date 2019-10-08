cc0722ef9f46ce95e45a2670fa41c932a3707918
Allah-The-Dev <pandeyneelkumar@gmail.com>
2019-10-08 14:00:15 +0530

    correcting request body key name for graphql api callk

--- src/main/java/com/neelesh/pocgraphql/DemoBusinessApi.java
+++ src/main/java/com/neelesh/pocgraphql/DemoBusinessApi.java
@@ -50,7 +50,7 @@ class DemoBusinessApi {
         // body  
         MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
         map.add("query", getQueryForBranches());
-        map.add("variable", "{\"url\":"+gitHubDetails.getRepoUrl().replace(".git", "")+"}");
+        map.add("variables", "{\"url\":"+gitHubDetails.getRepoUrl().replace(".git", "")+"}");
 
         // httpentity to hold header and body
         HttpEntity<MultiValueMap<String,String>> entity = new HttpEntity<>(map,headers); 
@@ -105,7 +105,7 @@ class DemoBusinessApi {
         // body
         MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
         map.add("query", getQueryForTags());
-        map.add("variable", "{\"url\":"+gitHubDetails.getRepoUrl().replace(".git", "")+"}");
+        map.add("variables", "{\"url\":"+gitHubDetails.getRepoUrl().replace(".git", "")+"}");
 
         // httpentity to hold header and body
         HttpEntity<MultiValueMap<String,String>> entity = new HttpEntity<>(map,headers); 
