192483292e437a96fe718c9c7ae7ac832d9c8b57
Allah-The-Dev <pandeyneelkumar@gmail.com>
2019-10-12 14:14:18 +0530

    removing unnecessary import for MultiValueMap

--- src/main/java/com/neelesh/pocgraphql/DemoBusinessApi.java
+++ src/main/java/com/neelesh/pocgraphql/DemoBusinessApi.java
@@ -15,8 +15,6 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 @Service
