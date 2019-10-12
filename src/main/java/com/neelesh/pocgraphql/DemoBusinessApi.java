5c5b7044786bd46d9950df83359d8abd4045dd4d
Allah-The-Dev <pandeyneelkumar@gmail.com>
2019-10-12 14:42:30 +0530

    adding variables in query and removing unnecessay code

--- src/main/java/com/neelesh/pocgraphql/DemoBusinessApi.java
+++ src/main/java/com/neelesh/pocgraphql/DemoBusinessApi.java
@@ -20,89 +20,81 @@ import org.springframework.web.client.RestTemplate;
 @Service
 class DemoBusinessApi {
 
-    private static Logger logger = LoggerFactory.getLogger(DemoBusinessApi.class);
+  private static Logger logger = LoggerFactory.getLogger(DemoBusinessApi.class);
 
-    private final RestTemplate restTemplate;
+  private final RestTemplate restTemplate;
 
-    public DemoBusinessApi(RestTemplateBuilder restTemplateBuilder) {
-        this.restTemplate = restTemplateBuilder.build();
-    }
+  public DemoBusinessApi(RestTemplateBuilder restTemplateBuilder) {
+      this.restTemplate = restTemplateBuilder.build();
+  }
 
-    public Map<String, List<String>> getGitHubBranchesAndTags(GitHubDetails gitHubDetails) {
+  public Map<String, List<String>> getGitHubBranchesAndTags(GitHubDetails gitHubDetails) {
 
-        // map of list to store 2 list 
-        // one for branches and one for tags
-        Map<String, List<String>> mapOfBranchAndTag = new HashMap<>();
-        mapOfBranchAndTag.put("branches", getRefs(gitHubDetails,"heads"));
-        mapOfBranchAndTag.put("tags",getRefs(gitHubDetails,"tags"));
-        return mapOfBranchAndTag;
-    }
+      // map of list to store 2 list 
+      // one for branches and one for tags
+      Map<String, List<String>> mapOfBranchAndTag = new HashMap<>();
+      mapOfBranchAndTag.put("branches", getRefs(gitHubDetails,"heads"));
+      mapOfBranchAndTag.put("tags",getRefs(gitHubDetails,"tags"));
+      return mapOfBranchAndTag;
+  }
 
-    private List<String> getRefs(GitHubDetails gitHubDetails,String refName) {
+  private List<String> getRefs(GitHubDetails gitHubDetails,String refName) {
 
-        // return object of list of branches
-        List<String> listOfRefs = new ArrayList<>();
+      // return object of list of branches
+      List<String> listOfRefs = new ArrayList<>();
 
-        // header
-        HttpHeaders requestHeader = new HttpHeaders();
-        requestHeader.set("Authorization", "bearer "+gitHubDetails.getAccessToken());
+      // header
+      HttpHeaders requestHeader = new HttpHeaders();
+      requestHeader.set("Authorization", "bearer "+gitHubDetails.getAccessToken());
 
-        // body  
-        // MultivalueMap didn't work here,
-        JSONObject requestBodyJSON = new JSONObject();
-        requestBodyJSON.put("query", getQueryForRefs(refName,gitHubDetails));
+      // body  
+      // MultivalueMap didn't work here,
+      JSONObject requestBodyJSON = new JSONObject();
+      requestBodyJSON.put("query", getQueryForRefs(refName,gitHubDetails));
+      requestBodyJSON.put("variables", "{url:\""+gitHubDetails.getRepoUrl()+"\"}");
+      // requestBodyMap.add("variables", "{url:\""+gitHubDetails.getRepoUrl()+"\"}");
 
-        // requestBodyMap.add("variables", "{url:\""+gitHubDetails.getRepoUrl()+"\"}");
+      // httpentity to hold header and body
+      HttpEntity<String> requestEntity = new HttpEntity<>(requestBodyJSON.toString(),requestHeader); 
+    
+      // restcall
+      ResponseEntity<String> responseForGetRefs = restTemplate.exchange("https://api.github.com/graphql",
+                                                                  HttpMethod.POST,requestEntity, String.class);
 
-        // httpentity to hold header and body
-        HttpEntity<String> requestEntity = new HttpEntity<>(requestBodyJSON.toString(),requestHeader); 
-     
-        // restcall
-        ResponseEntity<String> responseForGetRefs = restTemplate.exchange("https://api.github.com/graphql",
-                                                                    HttpMethod.POST,requestEntity, String.class);
+      // checking status
+      if(HttpStatus.OK.equals(responseForGetRefs.getStatusCode())){
+          logger.info("voila for refs");
+      }else{
+          logger.info("try again for refs");
+      }
 
-        // checking status
-        if(HttpStatus.OK.equals(responseForGetRefs.getStatusCode())){
-            logger.info("voila for refs");
-        }else{
-            logger.info("try again for refs");
-        }
+      // extracting result
+      return listOfRefs;
+  }
 
-        // extracting result
-        return listOfRefs;
-    }
-
-    private String getQueryForRefs(String refName, GitHubDetails gitHubDetails) {
-      String refPrefix = "\"refs/"+refName+"/\"";
-      // return String.join(
-      //   System.getProperty("line.separator"),
-      //   "{",
-      //     "repository(owner:\"Allah-The-Dev\",name:\"forms-reactive-start\"){",
-      //       "createdAt",
-      //     "}",
-      //   "}"
-      // );
-      return String.join(
-        System.getProperty("line.separator"), 
-        "{",
-            "resource(url:\""+gitHubDetails.getRepoUrl()+"\"){",
-              "... on Repository{",
-                "refs(refPrefix:"+refPrefix+",first:100,orderBy:{field:TAG_COMMIT_DATE,direction:DESC}){",
-                  "totalCount",
-                  "pageInfo{",
-                    "hasNextPage",
-                    "endCursor",
-                  "}",
-                  "edges{",
-                    "node{",
-                      "name",
-                    "}",
+  private String getQueryForRefs(String refName, GitHubDetails gitHubDetails) {
+    String refPrefix = "\"refs/"+refName+"/\"";
+    return String.join(
+      System.getProperty("line.separator"), 
+      "query($repoURL:URI!) {",
+          "resource(url:$repoURL){",
+            "... on Repository{",
+              "refs(refPrefix:"+refPrefix+",first:100,orderBy:{field:TAG_COMMIT_DATE,direction:DESC}){",
+                "totalCount",
+                "pageInfo{",
+                  "hasNext  Page",
+                  "endCursor",
+                "}",
+                "edges{",
+                  "node{",
+                    "name",
                   "}",
                 "}",
               "}",
             "}",
-          "}" 
-      );
-    }
+          "}",
+        "}" 
+    );
+  }
 
 }
