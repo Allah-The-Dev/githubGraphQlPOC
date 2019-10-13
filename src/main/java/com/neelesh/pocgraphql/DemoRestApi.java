82c1580ffe6aaf7881c18d848dd2ce2733e7e76a
Allah-The-Dev <pandeyneelkumar@gmail.com>
2019-10-13 18:50:56 +0530

    adding pathvariables to the rest end point

--- src/main/java/com/neelesh/pocgraphql/DemoRestApi.java
+++ src/main/java/com/neelesh/pocgraphql/DemoRestApi.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -42,10 +43,12 @@ public class DemoRestApi{
     }
 
     @PostMapping(path="/getDevDetails",produces = "application/json")
-    public ResponseEntity<Map<String,Map<String,String>>> getDevDetails(@RequestBody List<GitHubDetails> githubDetailsList){
+    public ResponseEntity<Map<String,Map<String,String>>> getDevDetails(@RequestBody List<GitHubDetails> githubDetailsList,
+                                                                        @PathVariable String since,
+                                                                        @PathVariable String until){
         Map<String,Map<String,String>> githubDevDetailsList = null;
         try {
-            githubDevDetailsList = demoBusinessApi.getGitHubDevDetails(githubDetailsList);
+            githubDevDetailsList = demoBusinessApi.getGitHubDevDetails(githubDetailsList,since,until);
         } catch (Exception e) {
            logger.info(e.toString());
         }
