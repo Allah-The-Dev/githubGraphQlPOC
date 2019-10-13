d066db2a08b6918a467173ea93f6d98d8802f0c0
Allah-The-Dev <pandeyneelkumar@gmail.com>
2019-10-13 18:50:09 +0530

    alignment for older code and new logic for getting devdetails

--- src/main/java/com/neelesh/pocgraphql/DemoBusinessApi.java
+++ src/main/java/com/neelesh/pocgraphql/DemoBusinessApi.java
@@ -1,9 +1,14 @@
 package com.neelesh.pocgraphql;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.json.JSONObject;
 import org.slf4j.Logger;
@@ -25,53 +30,51 @@ class DemoBusinessApi {
   private final RestTemplate restTemplate;
 
   public DemoBusinessApi(RestTemplateBuilder restTemplateBuilder) {
-      this.restTemplate = restTemplateBuilder.build();
+    this.restTemplate = restTemplateBuilder.build();
   }
 
   public Map<String, List<String>> getGitHubBranchesAndTags(GitHubDetails gitHubDetails) {
 
-      // map of list to store 2 list 
-      // one for branches and one for tags
-      Map<String, List<String>> mapOfBranchAndTag = new HashMap<>();
-      mapOfBranchAndTag.put("branches", getRefs(gitHubDetails,"heads"));
-      mapOfBranchAndTag.put("tags",getRefs(gitHubDetails,"tags"));
-      return mapOfBranchAndTag;
+    // map of list to store 2 list
+    // one for branches and one for tags
+    Map<String, List<String>> mapOfBranchAndTag = new HashMap<>();
+    mapOfBranchAndTag.put("branches", getRefs(gitHubDetails, "heads"));
+    mapOfBranchAndTag.put("tags", getRefs(gitHubDetails, "tags"));
+    return mapOfBranchAndTag;
   }
 
-  private List<String> getRefs(GitHubDetails gitHubDetails,String refName) {
+  private List<String> getRefs(GitHubDetails gitHubDetails, String refName) {
 
-      // return object of list of branches
-      List<String> listOfRefs = new ArrayList<>();
+    // return object of list of branches
+    List<String> listOfRefs = new ArrayList<>();
 
-      // header
-      HttpHeaders requestHeader = new HttpHeaders();
-      requestHeader.set("Authorization", "bearer "+gitHubDetails.getAccessToken());
+    // header
+    HttpHeaders requestHeader = new HttpHeaders();
+    requestHeader.set("Authorization", "bearer " + gitHubDetails.getAccessToken());
 
-      // body  
-      // MultivalueMap didn't work here,
-      JSONObject requestBodyJSON = new JSONObject();
-      requestBodyJSON.put("query", getQueryForRefs(refName));
-      requestBodyJSON.put("variables", "{url:\""+gitHubDetails.getRepoUrl()+"\"}");
-      // requestBodyMap.add("variables", "{url:\""+gitHubDetails.getRepoUrl()+"\"}");
+    // body
+    // MultivalueMap didn't work here,
+    JSONObject requestBodyJSON = new JSONObject();
+    requestBodyJSON.put("query", getQueryForRefs(refName));
+    requestBodyJSON.put("variables", "{url:\"" + gitHubDetails.getRepoUrl() + "\"}");
+    // requestBodyMap.add("variables", "{url:\""+gitHubDetails.getRepoUrl()+"\"}");
 
-      // httpentity to hold header and body
-      HttpEntity<String> requestEntity = new HttpEntity<>(requestBodyJSON.toString(),requestHeader); 
-    
-      // restcall
-      ResponseEntity<String> responseForGetRefs = restTemplate.exchange("https://api.github.com/graphql",
-                                                                  HttpMethod.POST,
-                                                                  requestEntity, 
-                                                                  String.class);
+    // httpentity to hold header and body
+    HttpEntity<String> requestEntity = new HttpEntity<>(requestBodyJSON.toString(), requestHeader);
 
-      // checking status
-      if(HttpStatus.OK.equals(responseForGetRefs.getStatusCode())){
-          logger.info("voila for refs");
-      }else{
-          logger.info("try again for refs");
-      }
+    // restcall
+    ResponseEntity<String> responseForGetRefs = restTemplate.exchange("https://api.github.com/graphql", HttpMethod.POST,
+        requestEntity, String.class);
 
-      // extracting result
-      return listOfRefs;
+    // checking status
+    if (HttpStatus.OK.equals(responseForGetRefs.getStatusCode())) {
+      logger.info("voila for refs");
+    } else {
+      logger.info("try again for refs");
+    }
+
+    // extracting result
+    return listOfRefs;
   }
 
   private String getQueryForRefs(String refName) {
@@ -99,72 +102,52 @@ class DemoBusinessApi {
     );
   }
 
-public Map<String, Map<String,String>> getGitHubDevDetails(List<GitHubDetails> githubDetailsList) {
-  Map<String,Map<String,String>> devDetailsMap = new HashMap<>();
-  githubDetailsList.forEach(singleGithubDetail -> {
-    getDetailsForSingleRepo(singleGithubDetail);
-  });
-  return devDetailsMap;
-}
 
-private void getDetailsForSingleRepo(GitHubDetails singleGithubDetail) {
-  //request header
-  HttpHeaders requestHeader = new HttpHeaders();
-  requestHeader.set("Authorization", "bearer "+singleGithubDetail.getAccessToken());
-  //request body
-  JSONObject requestBodyJSON = new JSONObject();
-  requestBodyJSON.put("query",getQueryForCommitDetails());
-  requestBodyJSON.put("variables","{url:\""+singleGithubDetail.getRepoUrl()+"\"}");
-  //request entity
-  HttpEntity<String> requestEntity = new HttpEntity<>(requestBodyJSON.toString(),requestHeader);
-  //http post call
-  ResponseEntity<JSONObject> responseEntity = restTemplate.exchange("https://api.github.com/graphql",
-                                                        HttpMethod.POST,
-                                                        requestEntity,
-                                                        JSONObject.class);
-  //checking status
-  if(HttpStatus.OK.equals(responseEntity.getStatusCode())){
-    logger.info("voila");
+  public Map<String, Map<String, String>> getGitHubDevDetails(List<GitHubDetails> githubDetailsList, String since, String until) {
+    Map<String, Map<String, String>> devDetailsMap = new HashMap<>();
+    githubDetailsList.forEach(singleGithubDetail -> {
+      getDetailsForSingleRepo(singleGithubDetail);
+    });
+    return devDetailsMap;
   }
-}
+
+  private void getDetailsForSingleRepo(GitHubDetails singleGithubDetail) {
+    // request header
+    HttpHeaders requestHeader = new HttpHeaders();
+    requestHeader.set("Authorization", "bearer " + singleGithubDetail.getAccessToken());
+    // request body
+    JSONObject requestBodyJSON = new JSONObject();
+    requestBodyJSON.put("query", getQueryForCommitDetails());
+    requestBodyJSON.put("variables", "{url:\"" + singleGithubDetail.getRepoUrl() + "\"}");
+    // request entity
+    HttpEntity<String> requestEntity = new HttpEntity<>(requestBodyJSON.toString(), requestHeader);
+    // http post call
+    ResponseEntity<JSONObject> responseEntity = restTemplate.exchange("https://api.github.com/graphql", HttpMethod.POST,
+        requestEntity, JSONObject.class);
+    // checking status
+    if (HttpStatus.OK.equals(responseEntity.getStatusCode())) {
+      logger.info("voila");
+    }
+  }
 
   private String getQueryForCommitDetails() {
-    return String.join(
-      System.getProperty("line.separator"),
-      "query($url:URI!) {",
-        "resource(url:$url) {",
-          "... on Repository {",
-           "refs(refPrefix:\"refs/heads/\",first:100,orderBy:{field:TAG_COMMIT_DATE,direction:DESC}){",
-             "pageInfo{",
-               "hasNextPage",
-               "endCursor",
-             "}",
-             "... commitQueryOnRefConn",
-           "}",
-          "}",
-        "}",
-       "}",
-       "fragment commitQueryOnRefConn on RefConnection{",
-        "edges{",
-         "node{",
-           "target{",
-             "... on Commit{",
-               "author{",
-                 "name",
-                 "email",
-               "}",
-               "committedDate",
-               "additions",
-               "deletions",
-               "parents(first:5){",
-                 "totalCount",
-               "}",
-             "}",
-           "}",
-         "}",
-        "}",
-       "}"
-    );
+    List<String> listOfQueryLines = null;
+    try {
+      listOfQueryLines = (List<String>) Files.readAllLines(Paths.get("src/main/resources/QueryForCommitHistory.txt"),StandardCharsets.UTF_8)
+                                              .stream()
+                                              .map(line -> line.trim())
+                                              .collect(Collectors.toList());
+    } catch (IOException e) {
+      logger.info(e.toString());
+    }
+    if(listOfQueryLines !=null && !listOfQueryLines.isEmpty()){
+      return String.join(
+        System.getProperty("line.separator"),
+        listOfQueryLines
+      );
+    }else{
+      return "NA";
+    }
   }
 
 }
